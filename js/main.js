f271aa9d75603046cda14a126b88f1684daea24d
senri <senrimeta@gmail.com>
2021-02-12 12:43:09 +0900

    トップバースクロール対応

--- js/main.js
+++ js/main.js
@@ -9,3 +9,12 @@ $(function() {
       }
   });
 });
+var barPos = $( '.top-bar' ).offset().top; // トップバーの位置
+var barHeight = $( '.top-bar' ).outerHeight(); // トップバーの高さ
+$( window ).on( 'scroll', function() {
+    if ( 40 < $( this ).scrollTop() ) { // 1000px以上スクロールしたら
+      $( '.top-bar' ).addClass( 'fixed' );
+    } else {
+      $( '.top-bar' ).removeClass( 'fixed' );
+    }
+  });
