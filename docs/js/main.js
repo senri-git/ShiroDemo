08a03b372477b4900af932f3d062bc3168109bc6
senri <senrimeta@gmail.com>
2021-02-14 14:33:10 +0900

    公開用初期コミット

---
+++ docs/js/main.js
@@ -0,0 +1,51 @@
+$(function() {
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+          $('.gray-out').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+          $('.gray-out').removeClass('active');
+      }
+  });
+  $('.gray-out').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+          $('.hamburger').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+          $('.hamburger').removeClass('active');
+      }
+  });
+  $('.globalMenuSp').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.gray-out').addClass('active');
+          $('.hamburger').addClass('active');
+      } else {
+          $('.gray-out').removeClass('active');
+          $('.hamburger').removeClass('active');
+      }
+  });
+});
+var barPos = $( '.top-bar' ).offset().top; // トップバーの位置
+var barHeight = $( '.top-bar' ).outerHeight(); // トップバーの高さ
+$( window ).on( 'scroll', function() {
+    if ( 40 < $( this ).scrollTop() ) { // 1000px以上スクロールしたら
+      $( '.top-bar' ).addClass( 'fixed' );
+    } else {
+      $( '.top-bar' ).removeClass( 'fixed' );
+    }
+  });
+
+// 横スクロールヒント
+  window.addEventListener('DOMContentLoaded', function(){
+    new ScrollHint('.gallery');
+    new ScrollHint('.florists');
+
+  });
